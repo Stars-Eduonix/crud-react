fa56ea5f57eb65310380b3f187d527e952b28cc5
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-09-02 21:40:07 +0530

    inidiv

--- src/Components/DisplayBillionair.js
+++ src/Components/DisplayBillionair.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const DisplayBillionair = () => {
 
     let [billionair, setBillionair] = useState([]);
+    let [billionairDetails, setBillionairDetails] = useState({});
 
 
     useEffect(() => {
@@ -18,17 +19,41 @@ const DisplayBillionair = () => {
         return firstLetter + rest;
     }
 
+    function getBillionairDetails(id){
+         axios.get(`http://localhost:5000/view/id/${id}`)
+            .then((response) => setBillionairDetails(response.data))
+            .catch((err) => console.log(err));
+    }
+
 
 
     return(
-        <div className="display-billionair">
-                 {
-                    billionair.length > 0  && (
-                        billionair.map((billi) => (
-                              <h3 key={billi._id}> {makeCapital(billi.name)} </h3>
+        <div className="display">
+            <div className="display-billionair">
+                    {
+                        billionair.length > 0  && (
+                            billionair.map((billi) => (
+                                <h3 onClick={()=>getBillionairDetails(billi._id)} key={billi._id}> {makeCapital(billi.name)} </h3>
+                            )
+                        ))
+                    }
+                    
+            </div>
+            <div>
+                  {
+                        Object.keys(billionairDetails).length > 0 && (
+                            <div className="billionair-details">
+                                <h2>{makeCapital(billionairDetails.name)}</h2>
+                                <h3>Age: {billionairDetails.age}</h3>
+                                <h3>Wealth: {billionairDetails.wealth} </h3>
+
+                            </div>
+
                         )
-                    ))
-                 }
+                  }
+                  
+            </div>
+
         </div>
     )
 }
